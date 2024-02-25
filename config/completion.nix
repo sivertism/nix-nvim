0d666b9240ad8c8f9c80cf7628b8de9f96845236
sivertism <10866270+sivertism@users.noreply.github.com>
2024-02-25 16:48:41 +0100

    Commented out luasnip
    Luasnip was failing every time I hit <Tab>

--- config/completion.nix
+++ config/completion.nix
@@ -7,29 +7,29 @@
         {name = "nvim_lsp";}
         {name = "path";}
         {name = "buffer";}
-        {name = "luasnip";}
+        #{name = "luasnip";} # Causes issues. Haven't bothered to fix it
       ];
 
       mapping = {
         "<CR>" = "cmp.mapping.confirm({ select = true })";
-        "<Tab>" = {
-          action = ''
-            function(fallback)
-              if cmp.visible() then
-                cmp.select_next_item()
-              elseif luasnip.expandable() then
-                luasnip.expand()
-              elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-              elseif check_backspace() then
-                fallback()
-              else
-                fallback()
-              end
-            end
-          '';
-          modes = [ "i" "s" ];
-        };
+        #"<Tab>" = {
+        #  action = ''
+        #    function(fallback)
+        #      if cmp.visible() then
+        #        cmp.select_next_item()
+        #      elseif luasnip.expandable() then
+        #        luasnip.expand()
+        #      elseif luasnip.expand_or_jumpable() then
+        #        luasnip.expand_or_jump()
+        #      elseif check_backspace() then
+        #        fallback()
+        #      else
+        #        fallback()
+        #      end
+        #    end
+        #  '';
+        #  modes = [ "i" "s" ];
+        #};
       };
     };
 }
